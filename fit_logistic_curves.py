26574476849d39234bc8ea4d276a2d3a46bc7663
Joshua Fan <joshuayfan@hotmail.com>
2021-10-12 18:16:23 -0400

    Add data files

--- fit_logistic_curves.py
+++ fit_logistic_curves.py
@@ -1,35 +1,29 @@
 """
 For each county/year and crop progress variable in the crop yield dataset, fit a logistic curve to the crop progress data over the course of the year.
-This curve is then used to interpolate missing values for that variable.
+This curve is then used to interpolate missing values for that variable. Example plots are outputted to the "plots" directory (which will be created)
 """
-import argparse
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import os
-import torch
-import visualization_utils
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_absolute_error as MAE
 from scipy.optimize import curve_fit
 
 # Dataset files
-DATASET_FILE = "/mnt/beegfs/bulk/mirror/jyf6/datasets/crop_forecast/data/combined_dataset_weekly.npz"  # Input dataset (compressed)
-OUTPUT_FILE = '/mnt/beegfs/bulk/mirror/jyf6/datasets/crop_forecast/data/combined_dataset_weekly_logistic_interpolation'  # File path to output dataset to
+DATASET_FILE = "combined_dataset_weekly_truncated.npz"  # Input dataset (compressed)
+OUTPUT_FILE = 'combined_dataset_weekly_logistic_interpolation'  # File path to output dataset to
 
-# Crop indices and names (outputs)
+# Column indices of different crop types (outputs)
 CROP_INDICES = list(range(2, 8))
 OUTPUT_NAMES = ['corn', 'upland_cotton', 'sorghum', 'soybeans', 'spring_wheat', 'winter_wheat']
 
-# List of progress indices
+# Column indices of the progress variables for each crop
 PROGRESS_INDICES_WEEKLY = {'corn': list(range(1464, 1880)),
                            'upland_cotton': list(range(2140, 2556)),
                            'sorghum': list(range(2816, 3232)),
                            'soybeans': list(range(3492, 4012)),
                            'spring_wheat': list(range(4948, 5364)),
                            'winter_wheat': list(range(5624, 6196))}
-DATASET_CSV_FILE = "/mnt/beegfs/bulk/mirror/jyf6/datasets/crop_forecast/data/combined_dataset_weekly_1981-2020.csv"
+DATASET_CSV_FILE = "combined_dataset_weekly_1981-2020_truncated.csv"
 COLUMN_NAMES = pd.read_csv(DATASET_CSV_FILE, index_col=False, nrows=0).columns.tolist()  # list of columns
 TIME_INTERVALS = 52  # number of weeks in year
 
@@ -91,12 +85,12 @@ for i in range(data.shape[0]):
             data[i, var_start_idx:var_start_idx+TIME_INTERVALS][missing_progress_data] = fitted_data[missing_progress_data].astype(int)
 
             # Plot example curves for some datapoints
-            if i % 177 == 0:
+            if i % 17 == 0:
                 plt.title("Crop progress (blue) and logistic curve (red): year " + str(year) + ", county " + str(county))
                 plt.scatter(xdata, ydata, c='b', label='data')
                 plt.plot(range(TIME_INTERVALS), logistic(range(TIME_INTERVALS), *popt), 'r-', label='fit')
                 plt.legend()
-                county_dir = os.path.join("exploratory/progress_curves", str(county), str(year))
+                county_dir = os.path.join("plots/progress_curves", str(county), str(year))
                 if not os.path.exists(county_dir):
                     os.makedirs(county_dir)
                 plt.savefig(os.path.join(county_dir, "progress_" + progress_col + "_county_" + str(county) + "_year_" + str(year) + ".png"))
